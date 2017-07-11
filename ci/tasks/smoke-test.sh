16979743e1673f9bff278f8e640acec0ea1ad376
Billy Campoli <william.campoli@logimethods.com>
2017-07-11 16:18:04 -0400

    fixed pipeline again again again

--- ci/tasks/smoke-test.sh
+++ ci/tasks/smoke-test.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+apt-get update
+apt-get install -y curl
+
 if [ -z $ATTENDEE_SERVICE_URL ]; then
   echo "ATTENDEE_SERVICE_URL not set"
   exit 1
@@ -11,8 +14,5 @@ pushd attendee-service-source
   echo "Running smoke tests for Attendee Service deployed at $ATTENDEE_SERVICE_URL"
   smoke-tests/bin/test $ATTENDEE_SERVICE_URL
 popd
-
-apt-get update
-apt-get install -y curl
 
 exit 0
