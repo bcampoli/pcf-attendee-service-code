09450265c4f5406e123a45f0bf9c86ca66db4d4f
Billy Campoli <william.campoli@logimethods.com>
2017-07-13 09:10:52 -0400

    fixed scripts

--- ci/tasks/remove_old.sh
+++ ci/tasks/remove_old.sh
@@ -1,18 +1,20 @@
+#!/bin/bash
 set -ex
 
 pwd
 env
 
 echo "current apps and routes:"
-cf apps
+
 
 cf api $CF_API --skip-ssl-validation
 
 cf login -u $CF_USER -p $CF_PWD -o "$CF_ORG" -s "$CF_SPACE"
+cf apps
 echo "Un-mapping old version of app:"
-cf unmap-route $CF_APP_SUFFIX $CF_DOMAIN -n $CF_ROUTE
+cf unmap-route $CF_APP_SUFFIX $CF_DOMAIN -n $CF_APP_SUFFIX-$CF_ROUTE
 echo "Un-mapping new version from temp domain:"
-cf unmap-route $CF_APP_SUFFIX_TEMP $CF_DOMAIN -n $CF_ROUTE-temp
+cf unmap-route $CF_APP_SUFFIX_TEMP $CF_DOMAIN -n $CF_APP_SUFFIX-$CF_ROUTE-temp
 cf routes
 echo "deleting old app:"
 cf delete $CF_APP_SUFFIX
